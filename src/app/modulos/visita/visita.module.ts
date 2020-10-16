96ae6c784788dcdfd9db6bdd525d2d89791cda9c
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-16 11:43:28 -0300

    creado módulos de visita y usuario

---
+++ src/app/modulos/visita/visita.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VisitaRoutingModule } from './visita-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VisitaRoutingModule
+  ]
+})
+export class VisitaModule { }

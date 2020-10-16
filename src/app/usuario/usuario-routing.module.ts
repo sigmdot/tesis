945e05bc80ce14d71c2a1e51d2dad51a7f0614cc
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-16 18:13:55 -0300

    Fix routing
    Routing arreglado para futuras referencias

--- src/app/usuario/usuario-routing.module.ts
+++ src/app/usuario/usuario-routing.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsuarioComponent } from './usuario.component';
 import { HistorialUsuarioComponent } from './views/historial-usuario/historial-usuario.component';
 import { HomeComponent } from './views/home/home.component';
 import { PerfilUsuarioComponent } from './views/perfil-usuario/perfil-usuario.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'historial',
-    component: HistorialUsuarioComponent
-  },
-  {
-    path: 'perfil',
-    component: PerfilUsuarioComponent
+    path: '',
+    component: UsuarioComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'historial',
+        component: HistorialUsuarioComponent
+      },
+      {
+        path: 'perfil',
+        component: PerfilUsuarioComponent
+      }
+    ]
   }
 ];
 

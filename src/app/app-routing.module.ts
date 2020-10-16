e7f39d0eaefd877065735b38453c6570e3963d55
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-16 11:49:01 -0300

    agregado routing para los modulos
    Visita y usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modulos/visita/visita.module').then(module => module.VisitaModule)
+  },
+  {
+    path:'desktop',
+    loadChildren: () => import('./modulos/usuario/usuario.module').then(module => module.UsuarioModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

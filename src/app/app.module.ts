9ae0dc466bbe62c244df2c53463d16e84a140017
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-16 11:57:15 -0300

    Generado vista "Landing"
    Vista de recibimiento de usuarios creada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Iconos
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LandingComponent } from './views/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,

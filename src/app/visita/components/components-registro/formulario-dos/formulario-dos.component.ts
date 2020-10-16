58d934b138d57bfb4ab3f100baee597b4c487164
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-16 16:29:32 -0300

    editado y agregado formulario dos (Registro)
    Formulario dos creado

--- src/app/visita/components/components-registro/formulario-dos/formulario-dos.component.ts
+++ src/app/visita/components/components-registro/formulario-dos/formulario-dos.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-dos.component.css']
 })
 export class FormularioDosComponent implements OnInit {
+  opcionesRegion =[
+    {
+      value: 1,
+      name: 'I - Tarapacá'
+    },
+    {
+      value: 2,
+      name: 'II - Antofagasta'
+    },
+    {
+      value: 3,
+      name: 'III - Atacama'
+    },
+    {
+      value: 4,
+      name: 'IV - Coquimbo'
+    },
+    {
+      value: 5,
+      name: 'V - Valparaíso'
+    },
+    {
+      value: 6,
+      name: 'VI - Libertador Bernardo O\'Higgins'
+    },
+    {
+      value: 7,
+      name: 'VII - Maule'
+    },
+    {
+      value: 8,
+      name: 'VIII - Concepción'
+    },
+    {
+      value: 9,
+      name: 'IX - Araucanía'
+    }
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validacionFormularioDos(){
+    console.log('Validacion segundo formulario');
   }
 
 }

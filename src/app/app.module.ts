5f42fa32e34f6bf8dd4f6aa5abde426f2cb7414f
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-16 11:35:00 -0300

    Se limpia proyecto y agrega librerias
    Bootstrap,Angular material,Mapbox,Firebase,toast,Font-awesome.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Iconos
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxKey: 'pk.eyJ1Ijoic2lnbWRvdCIsImEiOiJja2Z0d3V6aHEwbWV0MnNxcWlmc3Y0eGlnIn0.Vxw5oue-Z9_ia3tW1zifkA'
 };

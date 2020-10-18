b6c1401a1387ba6f5000d3bc6ab01e7003a2ec2b
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-18 13:05:10 -0300

    Se incorpora funciones al provider

--- src/app/core/service-providers/collecion-usuarios/collecion-usuarios.service.ts
+++ src/app/core/service-providers/collecion-usuarios/collecion-usuarios.service.ts
@@ -7,4 +7,16 @@ import { FirestoreService } from '@core/services/firestore/firestore.service';
 export class CollecionUsuariosService {
 
   constructor(private firestoreSvc: FirestoreService) { }
+
+  // tslint:disable-next-line: typedef
+  public getAllUsuario(){
+    // any remplazarlo por modelo
+    return this.firestoreSvc.snapshotCollection<any>(null, 'usuarios');
+  }
+
+  // tslint:disable-next-line: typedef
+  public getAviso(id: string){
+    return this.firestoreSvc.snapshotDocument<any>(null, `usuarios/${id}`);
+  }
+
 }

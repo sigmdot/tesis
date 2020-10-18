a53a3a5c9cd864d721d708a7d54072365107a021
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-18 12:38:26 -0300

    Arreglado formularios para registrar respuestas!
    Ahora efectivamente los primeros dos formularios guardan respuestas

--- src/app/visita/components/components-registro/formulario-uno/formulario-uno.component.ts
+++ src/app/visita/components/components-registro/formulario-uno/formulario-uno.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-formulario-uno',
@@ -7,11 +9,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioUnoComponent implements OnInit {
 
-  constructor() { }
+  formularioRegistro: FormGroup = new FormGroup({
+    correo: new FormControl('', [Validators.required, ]),
+    correoConf: new FormControl('', Validators.required),
+    rut: new FormControl('', Validators.required),
+    pass: new FormControl('', Validators.minLength(7)),
+    passConf: new FormControl('', Validators.required)
+  }, {
+    validators: [this.formValidator]
+  });
+
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
-  validacionFormularioUno(){
-    console.log('Validado');
+  // tslint:disable-next-line: typedef
+  validacionFormularioUno() {
+    const correo = this.formularioRegistro.value.correo;
+    const correoConf = this.formularioRegistro.value.correoConf;
+    const rut = this.formularioRegistro.value.rut;
+    const pass = this.formularioRegistro.value.pass;
+    const passConf = this.formularioRegistro.value.passConf;
+
+    console.log(this.formularioRegistro);
+
+    if (this.formularioRegistro.invalid) {
+      if (correo === '') {
+        this.toastr.warning('Correo vacio');
+      }
+      if (correoConf === '') {
+        this.toastr.warning('Debe ingresar un correo de confirmación');
+      }
+      if (pass === '') {
+        this.toastr.warning('Debe ingresar una contraseña');
+      }
+      if (passConf === '') {
+        this.toastr.warning('Debe confirmar su contraseña');
+      }
+      if (rut === '') {
+        this.toastr.warning('Debe ingresar su rut');
+      }
+      if (correo !== correoConf) {
+        this.toastr.warning('Correos diferentes');
+      }
+      if (pass !== passConf) {
+        this.toastr.warning('Contraseñas ingresadas diferentes');
+      }
+      return;
+    }
+    if (correo !== correoConf) {
+      this.toastr.warning('Correos no coinciden');
+    }
+    if (pass !== passConf) {
+      this.toastr.warning('Contraseñas no coinciden');
+    }
+    if (!this.validarut(rut)) {
+      this.toastr.warning('Rut inválido');
+    }
+    console.log(correo, correoConf, rut, pass, passConf);
+  }
+  // tslint:disable-next-line: typedef
+  validarut(rut: string) {
+    return true;
+  }
+
+  // tslint:disable-next-line: typedef
+  formValidator(g: FormGroup) {
+    // tslint:disable-next-line: max-line-length
+    if ((g.get('pass').value != null) && (g.get('correo').value != null) && (g.get('passConf').value != null) && (g.get('correoConf').value != null)) {
+      if (((g.get('pass').value) === (g.get('passConf').value)) && ((g.get('correo').value) === (g.get('correoConf').value))) {
+        return null;
+      }
+    }
+    return { datosEnNulo: true };
   }
 }

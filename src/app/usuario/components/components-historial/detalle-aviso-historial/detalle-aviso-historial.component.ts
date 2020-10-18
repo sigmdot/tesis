4fd4fc13a47e3b677edbad335aa32836fa4c1efc
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-18 15:14:11 -0300

    implementado sistema de input y output para obtener indice
    Se implementa salid y entrada de datos

--- src/app/usuario/components/components-historial/detalle-aviso-historial/detalle-aviso-historial.component.ts
+++ src/app/usuario/components/components-historial/detalle-aviso-historial/detalle-aviso-historial.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-detalle-aviso-historial',
   templateUrl: './detalle-aviso-historial.component.html',
   styleUrls: ['./detalle-aviso-historial.component.css']
 })
-export class DetalleAvisoHistorialComponent implements OnInit {
-
+export class DetalleAvisoHistorialComponent implements OnInit, OnChanges {
+  @Input() avisoDetalle: any;
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 
   ngOnInit(): void {
   }
--- src/app/usuario/components/components-historial/lista-historial/lista-historial.component.ts
+++ src/app/usuario/components/components-historial/lista-historial/lista-historial.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-historial.component.css']
 })
 export class ListaHistorialComponent implements OnInit {
-
+  indice;
   constructor() { }
-
+  arrayOne(n: number): any[] {
+    return Array(n);
+  }
   ngOnInit(): void {
+  }
+  capturarIndice(indice: any){
+    console.log(indice);
+    this.indice = indice;
   }
 
 }

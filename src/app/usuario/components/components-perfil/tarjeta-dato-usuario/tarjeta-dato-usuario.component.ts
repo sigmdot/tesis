c74a137c5588f2b9feedbc1182a8156f284f3f7b
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-10-18 23:27:36 -0300

    actualizacion perfil
    Actualizacion traspaso datos en perfil

--- src/app/usuario/components/components-perfil/tarjeta-dato-usuario/tarjeta-dato-usuario.component.ts
+++ src/app/usuario/components/components-perfil/tarjeta-dato-usuario/tarjeta-dato-usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faStreetView } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-tarjeta-dato-usuario',
@@ -7,6 +7,8 @@ import { faStreetView } from '@fortawesome/free-solid-svg-icons';
 })
 export class TarjetaDatoUsuarioComponent implements OnInit {
   faStreetView = faStreetView;
+  @Input() tipo: string;
+  @Input() dato: any;
   constructor() { }
 
   ngOnInit(): void {

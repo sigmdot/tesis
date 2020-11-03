43a0af9eb52373f5ecdfd28353b52a613cede8f4
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-11-03 20:03:09 -0300

    Lado empleador avisos 75% terminado
    Falta el 25 que es poner un titulo a las lista

--- src/app/usuario/components/components-historial/tarjeta-avisos-activos/tarjeta-avisos-activos.component.ts
+++ src/app/usuario/components/components-historial/tarjeta-avisos-activos/tarjeta-avisos-activos.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { AvisoTrabajo } from '@core/model/aviso-trabajo.model';
+import {faIdBadge, faTimes} from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-tarjeta-avisos-activos',
   templateUrl: './tarjeta-avisos-activos.component.html',
   styleUrls: ['./tarjeta-avisos-activos.component.css']
 })
 export class TarjetaAvisosActivosComponent implements OnInit {
-
+  faIdBadge = faIdBadge;
+  faTimes = faTimes;
+  @Input() aviso: AvisoTrabajo;
   constructor() { }
 
   ngOnInit(): void {

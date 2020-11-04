2fe6fe90abcb07b3382fda2cf4cddec9eadbc0f7
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-11-04 13:40:59 -0300

    modelado la tarjetas

--- src/app/usuario/components/components-historial/tarjeta-trabajos-activos-concretados/tarjeta-trabajos-activos-concretados.component.ts
+++ src/app/usuario/components/components-historial/tarjeta-trabajos-activos-concretados/tarjeta-trabajos-activos-concretados.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AvisoTrabajo } from '@core/model/aviso-trabajo.model';
+import { Trabajo } from '@core/model/trabajo.model';
+import { Usuario } from '@core/model/usuario.model';
+import { AvisosTrabajosService } from '@core/service-providers/avisos-trabajos/avisos-trabajos.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tarjeta-trabajos-activos-concretados',
   templateUrl: './tarjeta-trabajos-activos-concretados.component.html',
   styleUrls: ['./tarjeta-trabajos-activos-concretados.component.css']
 })
-export class TarjetaTrabajosActivosConcretadosComponent implements OnInit {
-
-  constructor() { }
+export class TarjetaTrabajosActivosConcretadosComponent implements OnInit, OnChanges {
+  @Input() trabajoActivo: Trabajo;
+  aviso$: Observable<AvisoTrabajo>;
+  trabajadoAsociado$: Observable<Usuario>;
+  constructor(private avisoSvc: AvisosTrabajosService) { }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.aviso$ = this.avisoSvc.getAviso(this.trabajoActivo.idAvisoAsociado);
+  }
 
   ngOnInit(): void {
   }

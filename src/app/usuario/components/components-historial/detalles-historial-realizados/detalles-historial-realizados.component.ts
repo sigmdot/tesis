b86cf61603ef8727cd8fe57e20ce31355eaaa600
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-11-05 17:46:00 -0300

    Actualizado algunos estilos

--- src/app/usuario/components/components-historial/detalles-historial-realizados/detalles-historial-realizados.component.ts
+++ src/app/usuario/components/components-historial/detalles-historial-realizados/detalles-historial-realizados.component.ts
@@ -1,22 +1,28 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewChecked, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { AvisoTrabajo } from '@core/model/aviso-trabajo.model';
 import { Trabajo } from '@core/model/trabajo.model';
 import { Usuario } from '@core/model/usuario.model';
 import { AvisosTrabajosService } from '@core/service-providers/avisos-trabajos/avisos-trabajos.service';
 import { CollecionUsuariosService } from '@core/service-providers/collecion-usuarios/collecion-usuarios.service';
-import { TrabajosCollecionService } from '@core/service-providers/trabajos-collecion/trabajos-collecion.service';
 import { Observable } from 'rxjs';
+import { MapaDetalleHistorialRealizadosTrabajosComponent } from '../mapa-detalle-historial-realizados-trabajos/mapa-detalle-historial-realizados-trabajos.component';
 
 @Component({
   selector: 'app-detalles-historial-realizados',
   templateUrl: './detalles-historial-realizados.component.html',
   styleUrls: ['./detalles-historial-realizados.component.css']
 })
-export class DetallesHistorialRealizadosComponent implements OnInit, OnChanges {
+export class DetallesHistorialRealizadosComponent implements OnInit, OnChanges , AfterViewChecked {
   @Input() trabajoDetalle: Trabajo;
   usuarioEmpleador$: Observable<Usuario>;
   avisoAsociado$: Observable<AvisoTrabajo>;
+  @ViewChild(MapaDetalleHistorialRealizadosTrabajosComponent) mapa: MapaDetalleHistorialRealizadosTrabajosComponent;
   constructor(private userColleSvc: CollecionUsuariosService, private avisoCollecSvc: AvisosTrabajosService) { }
+  ngAfterViewChecked(): void {
+    if((this.mapa.mapaDetalleHistorialRealizados !== null)&&(this.mapa.mapaDetalleHistorialRealizados !== undefined)){
+      this.mapa.mapaDetalleHistorialRealizados.resize();
+    }
+  }
   ngOnInit(): void {
   }
 

130a2e50d7f963e14fa170b9994a64f3b24ebe86
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-11-10 20:41:32 -0300

    actualizado

---
+++ src/app/core/variables/regiones.ts
@@ -0,0 +1,76 @@
+export const regiones = [
+    {
+      value: 1,
+      name: 'I - Tarapacá',
+      coords: [-69.785483, -20.254472]
+    },
+    {
+      value: 2,
+      name: 'II - Antofagasta',
+      coords: [-70.392731,-23.640835]
+    },
+    {
+      value: 3,
+      name: 'III - Atacama',
+      coords: [-70.332571,-27.375178]
+    },
+    {
+      value: 4,
+      name: 'IV - Coquimbo',
+      coords: []
+    },
+    {
+      value: 5,
+      name: 'V - Valparaíso',
+      coords: []
+    },
+    {
+      value: 6,
+      name: 'VI - Libertador Bernardo O\'Higgins',
+      coords: []
+    },
+    {
+      value: 7,
+      name: 'VII - Maule',
+      coords: []
+    },
+    {
+      value: 8,
+      name: 'VIII - Concepción',
+      coords: []
+    },
+    {
+      value: 9,
+      name: 'IX - Araucanía',
+      coords: []
+    },
+    {   
+        value: 10,
+        name: 'X - Región de los lagos',
+        coords: []
+    },
+    {
+        value: 11,
+        name: 'XI - Aysén del Gral. Carlos Ibáñez del Campo',
+        coords: []
+    },
+    {
+        value: 12,
+        name: 'XII - Magallanes y Antartica Chilena'
+    },
+    {
+      value: 13,
+      name: 'Región metropolitana',
+      coords: [-70.689409, -33.518071]
+    },
+    {
+        value: 14,
+        name: 'XIV - Región de los Rios',
+        coords: []
+    },
+    {
+        value: 15,
+        name: 'XV - Región Arica y Parinacota',
+        coords: []
+    }
+];

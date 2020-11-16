d5593216e7d89ea5c505de9bcac0760d1441b4fa
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-11-16 20:57:51 -0300

    update

--- src/app/usuario/components/components-perfil/modal-detalle-comentario/modal-detalle-comentario.component.ts
+++ src/app/usuario/components/components-perfil/modal-detalle-comentario/modal-detalle-comentario.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AvisoTrabajo } from '@core/model/aviso-trabajo.model';
+import { Comentario } from '@core/model/comentario.model';
+import { AvisosTrabajosService } from '@core/service-providers/avisos-trabajos/avisos-trabajos.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-modal-detalle-comentario',
   templateUrl: './modal-detalle-comentario.component.html',
   styleUrls: ['./modal-detalle-comentario.component.css']
 })
-export class ModalDetalleComentarioComponent implements OnInit {
+export class ModalDetalleComentarioComponent implements OnInit, OnChanges {
+  @Input() comentarioDetalle: Comentario;
+  aviso$: Observable<AvisoTrabajo>;
+  constructor( private avisoSvc: AvisosTrabajosService ) { 
 
-  constructor() { }
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    console.log(this.comentarioDetalle.idTrabajo);
+    this.aviso$ = this.avisoSvc.getAviso(this.comentarioDetalle.idTrabajo);
+  }
 
   ngOnInit(): void {
   }

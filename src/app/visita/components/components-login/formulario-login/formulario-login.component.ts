43a67719680a1fb9bed29563d75d147ffeac86b9
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-11-25 22:25:00 -0300

    udpate

--- src/app/visita/components/components-login/formulario-login/formulario-login.component.ts
+++ src/app/visita/components/components-login/formulario-login/formulario-login.component.ts
@@ -2,7 +2,8 @@ import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import {FireauthService} from '@core/services/fireauth/fireauth.service';
+import { FireauthService } from '@core/services/fireauth/fireauth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-formulario-login',
@@ -11,20 +12,29 @@ import {FireauthService} from '@core/services/fireauth/fireauth.service';
 })
 export class FormularioLoginComponent implements OnInit {
 
-  constructor(private authSvc: FireauthService, private router: Router) { }
+  constructor(private authSvc: FireauthService, private router: Router, private toastr: ToastrService) { }
   formLogin: FormGroup = new FormGroup({
     correo: new FormControl('', Validators.required),
-    pass: new FormControl('', Validators.required)  
+    pass: new FormControl('', Validators.required)
   });
-  ngOnInit(  ): void {
+  ngOnInit(): void {
   }
   // tslint:disable-next-line: typedef
-  login(){
-    const email = this.formLogin.value.correo
+  login() {
+    const email = this.formLogin.value.correo;
     const pass = this.formLogin.value.pass;
-    this.authSvc.loginEmailPass(email, pass).then((e)=>{
-      console.log(e);
+    this.authSvc.loginEmailPass(email, pass).then((e) => {
       this.router.navigateByUrl('/desktop/home');
+
+    }).catch(e => {
+      if(e.code === 'auth/invalid-email'){
+        this.toastr.warning('correo inválido','Error en los datos');
+      }
+      else{
+        this.toastr.warning('Error en los datos','Datos erroneos');
+      }
+      this.formLogin.reset('');
+      console.clear();
     });
   }
 
